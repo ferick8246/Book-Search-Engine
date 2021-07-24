ce6ffb59314cafd555af279069fe9d69ae19a9d3
Erick Flore <ferick8246@gmail.com>
2021-07-24 16:58:14 -0500

    mispellings

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -2,9 +2,9 @@
 import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
-import { useQuery, useMutation } from "@apollo/react-hooks";
+import { useQuery, useMutation } from "@apollo/client";
 import { GET_ME } from "../utils/queries";
-import { REMOVE_BOOK } from "../utils/mutations";
+import { REMOVE_BOOK } from "../utils/mutation";
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
