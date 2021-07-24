bc2cf1975269c14af1dc68f54b009322b947f068
Erick Flore <ferick8246@gmail.com>
2021-07-24 16:57:57 -0500

    mispellings

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,8 +3,8 @@ import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { useMutation } from '@apollo/react-hooks';
-import { LOGIN_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutation';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
